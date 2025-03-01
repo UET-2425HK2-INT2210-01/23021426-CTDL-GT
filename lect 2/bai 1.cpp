5cef6b0bd33513361ff7f4c1d53905a6b6238642
23021426-CTDL-GT <23021426@vnu.edu.vn>
2025-03-01 23:11:21 +0700

    Add files via upload

---
+++ lect 2/bai 1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int Pairs(int n, int a[]) {
+    int count = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] == a[j]) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    cout << Pairs(n, a) << endl;
+    return 0;
+}
---
+++ lect 2/bai 4.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int val;
+    Node* next;
+    Node(int v) : val(v), next(nullptr) {}
+};
+
+class Queue {
+private:
+    Node* front;
+    Node* rear;
+public:
+    Queue() : front(nullptr), rear(nullptr) {}
+
+    void enqueue(int x) {
+        Node* newNode = new Node(x);
+        if (!rear) {
+            front = rear = newNode;
+        }
+        else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+
+    void dequeue() {
+        if (!front) return;
+        Node* temp = front;
+        front = front->next;
+        if (!front) rear = nullptr;
+        delete temp;
+    }
+
+    void print() {
+        Node* temp = front;
+        while (temp) {
+            cout << temp->val << " ";
+            temp = temp->next;
+        }
+    }
+};
+
+int main() {
+    int n;
+    cin >> n;
+    Queue q;
+
+    for (int i = 0; i < n; i++) {
+        string op;
+        cin >> op;
+        if (op == "enqueue") {
+            int x;
+            cin >> x;
+            q.enqueue(x);
+        }
+        else if (op == "dequeue") {
+            q.dequeue();
+        }
+    }
+
+    q.print();
+    return 0;
+}
